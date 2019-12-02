5cd4bacc9447ab01a895506f0342b577b91d6114
supersammy70000 <lance.e.jordan@gmail.com>
2019-12-02 17:15:55 -0600

    Implemented deal class and link parsing. Vendor and content fields still need to be parsed.

--- ParseWebpage.py
+++ ParseWebpage.py
@@ -1,6 +1,23 @@
 import urllib.request
 import bs4 as bs
 
+class deal:
+    def __init__(self, title, link, content, vendor):
+        self.title = title
+        self.link = link
+        self.content = content
+        self.vendor = vendor
+    def getLink(self):
+        return self.link
+    def getTitle(self):
+        return self.title
+    def setTitle(self, title):
+        self.title = title
+    def getContent(self):
+        return self.content
+    def getVendor(self):
+        return self.vendor
+
 infile = urllib.request.urlopen("http://www.dealsea.com")
 data = infile.read().decode()
 f = open('dealsea.data', 'w')
@@ -17,6 +34,11 @@ soup2 = soup.findAll("div", class_="dealcontent")
 
 soup3 = []
 for i in soup2:
-    #print(i.strong.a.get_text())
-    soup3.append(i.strong.a.get_text())
-print(len(soup3),"element(s)")
+    title = i.strong.a.get_text()
+    link = i.strong.a.get('href')
+
+    soup3.append(deal(title,link,'test','test'))
+
+for i in soup3:
+    print(i.getLink(),i.getTitle())
+print(len(soup3), "element(s)")
