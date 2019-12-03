e4c21d2e9504cce36192d0bec625299f1353a188
supersammy70000 <lance.e.jordan@gmail.com>
2019-12-03 11:16:53 -0600

    Add Email sending functionality

--- ParseWebpage.py
+++ ParseWebpage.py
@@ -1,5 +1,6 @@
 import urllib.request
 import bs4 as bs
+import requests
 
 class deal:
     def __init__(self, title, link, content, vendor):
@@ -15,6 +16,15 @@ class deal:
         return self.content
     def getVendor(self):
         return self.vendor
+def getDealsText(deals, n):
+    length = len(deals)
+    text = ""
+    if length < n:
+        return
+    print("Title, vendor, URL, content")
+    for i in range(n):
+        text = text + "\n" + deals[i].getTitle() + " : " + deals[i].getVendor() + " : " + deals[i].getLink() + "\n" + deals[i].getContent() + "\n----------------------------------------"
+    return text
 def displayDeals(deals, n):
     length = len(deals)
     if length < n:
@@ -22,6 +32,18 @@ def displayDeals(deals, n):
     print("Title, vendor, URL, content")
     for i in range(n):
         print(deals[i].getTitle(),":",deals[i].getVendor(),":",deals[i].getLink(),":",deals[i].getContent())
+
+def sendDeals(deals):
+    f = open('mailgun.key')
+    key = f.read()
+    f.close()
+    return requests.post(
+        "https://api.mailgun.net/v3/sandboxd10bb35ab0c4461aabdc94d6fce977ac.mailgun.org/messages",
+        auth=("api", key),
+        data={"from": "Mailgun Sandbox <postmaster@sandboxd10bb35ab0c4461aabdc94d6fce977ac.mailgun.org>",
+            "to": "Lance Jordan <lance.e.jordan@gmail.com>",
+            "subject": "Dealsea top 10 deals",
+            "text": deals})
 
 infile = urllib.request.urlopen("http://www.dealsea.com")
 data = infile.read().decode()
@@ -35,10 +57,10 @@ f.close()
 
 soup = bs.BeautifulSoup(data, 'html.parser')
 
-soup2 = soup.findAll("div", class_="dealcontent")
+divSoup = soup.findAll("div", class_="dealcontent")
 
 dealSea = []
-for i in soup2:
+for i in divSoup:
     title = i.strong.a.get_text()
     link = i.strong.a.get('href')
     vendor = i.div.a.get_text()
@@ -47,3 +69,6 @@ for i in soup2:
     dealSea.append(deal(title,link,content,vendor))
 
 displayDeals(dealSea, 5)
+
+print(sendDeals(getDealsText(dealSea, 10)))
+
